502505be542eb399e0d5e858f9ecbcb81e115b1e
VictorWSC <159850756+VictorWSC@users.noreply.github.com>
2024-05-03 12:13:37 +1200

    s

---
+++ main.py
@@ -0,0 +1,24 @@
+#ask user their name and save it
+name=input ("What's your name") 
+#Greet the user and introduce the quiz
+print ("Welcome to my quiz,",name)
+print ("This quiz is about Animals")
+#Question1
+input ("Whats the animal with the longest life span?")
+#Answer 1
+print ("The answer was The Immortal Jellyfish")
+#Question2
+input ("Whats the heaviest animal")
+#Answer 2
+print ("The answer was Blue Whale")
+#Question3
+input ("Whats the oldest animal species")
+#Answer 3
+print ("The answer was Horseshoe Crab")
+#Question4
+input ("Whats the most dangerous animal")
+#Answer 4
+print ("The answer was Mosquito")
+#End the quiz
+print ("This is the end Thank you for participating in my quiz")
+
