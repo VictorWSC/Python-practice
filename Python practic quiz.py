1e6ea7ed1eef0a109b8abc7ce498a6ab10d97356
VictorWSC <159850756+VictorWSC@users.noreply.github.com>
2024-05-03 12:59:08 +1200

    '

--- Python practic quiz.py
+++ Python practic quiz.py
@@ -1,24 +1,40 @@
 #ask user their name and save it
-name=input ("What's your name") 
+name=input ("What's your name? ") 
 #Greet the user and introduce the quiz
 print ("Welcome to my quiz,",name)
 print ("This quiz is about Animals")
 #Question1
-input ("Whats the animal with the longest life span?")
+answer=input ("Whats the animal with the longest life span?")
 #Answer 1
-print ("The answer was The Immortal Jellyfish")
+if answer=="The Immortal Jellyfish":
+    print("correct")
+else :
+    print("Incorrect")
+    print ("The answer was The Immortal Jellyfish",answer)
 #Question2
-input ("Whats the heaviest animal")
+answer=input ("Whats the heaviest animal?")
 #Answer 2
-print ("The answer was Blue Whale")
+if answer=="Blue Whale":
+    print("correct")
+else :
+    print("Incorrect")
+    print ("The answer was Blue Whale",answer)
 #Question3
-input ("Whats the oldest animal species")
+answer=input ("Whats the oldest animal species?")
 #Answer 3
-print ("The answer was Horseshoe Crab")
+if answer=="Horseshoe Crab":
+    print("correct")
+else :
+    print("Incorrect")
+    print ("The answer was Horseshoe Crab",answer)
 #Question4
-input ("Whats the most dangerous animal")
+answer=input ("Whats the most dangerous animal?")
 #Answer 4
-print ("The answer was Mosquito")
+if answer=="Mosquito":
+    print("correct")
+else :
+    print("Incorrect")
+    print ("The answer was Mosquito",answer)
 #End the quiz
 print ("This is the end Thank you for participating in my quiz")
 
